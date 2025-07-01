0e8b068b229d08d51bee0c44b8bd3047fb14f3b5
AtharvBhagat <159274676+AtharvBhagat@users.noreply.github.com>
2025-07-01 19:43:44 +0530

    Add files via upload

---
+++ project/SQL-49/SQL-49.sql
@@ -0,0 +1,17 @@
+CREATE TABLE detail(
+rno TEXT PRIMARY KEY,
+name TEXT,
+city TEXT,
+D.O.B TEXT,
+admno INTEGER
+);
+
+INSERT INTO detail(rno,name,city,D.O.B,admno)VALUES
+('7118','Atharv','Chennai','17/4/2025',123),
+('7119','Samaviya','peshwar','20/6/2025',124),
+('7120','Ram','mumbai','23/8/2025',125);
+
+
+SELECT * FROM student;
+
+
